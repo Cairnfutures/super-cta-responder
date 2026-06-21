31f01f73901e2c717b9c42ed9b1fa0a21fee3f67
Louise Jones <louise@cairngormfutures.co.uk>
2026-06-21 13:36:16 +0100

    feat: build timestamp in footer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+  env: {
+    NEXT_PUBLIC_BUILD_TIME: new Date().toISOString().slice(0, 16).replace('T', ' ') + ' UTC',
+  },
 }
 module.exports = nextConfig
